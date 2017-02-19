751853638df63cd394e1ce08133a7e1ca8e0f0ec
naresh242 <naresh.srikakolapu@gmail.com>
2017-02-19 13:41:38 +0530

    new file
    URL can be any address over the network like:'https://goo.gl/vhm1eU'
    
    how to read data from CSV using PANDAS

---
+++ readcsv.py
@@ -0,0 +1,12 @@
+# Load CSV using Pandas from URL
+from pandas import read_csv
+url = ''
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(url, names=names)
+print(data.shape)
+
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+print(data.shape)
